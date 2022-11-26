355e04acd79058c8c4eaf49571a32c0776eba6ce
Gildo Costa Francisco Chauze <gildochauze@gmail.com>
2022-11-26 20:41:45 +0200

    Fix: Responsive layout to mobile and desktop

---
+++ README.md
@@ -0,0 +1,22 @@
+# Password Generator
+
+---
+
+A configurable and cryptographically secure random password generator that runs inside your browser.
+Looking to use Password Generator? Check [here](https://gildoch.github.io/password-generator/)!
+
+## About <a name = "about"></a>
+
+---
+
+Password Generator is a web app that generates random passwords inside your browser. Features include:
+
+- Passwords never leave your device — all data stays in your browser and no network requests are made.
+- Password generation using cryptographically secure randomness. Generated passwords are impossible to predict or manipulate.
+- Different options to control how passwords are generated, including number of digits.
+
+## License <a name = "license"></a>
+
+---
+
+Password Generator is provided under the terms of the MIT License. See the LICENSE [LICENSE](https://www.example.com) file for details.
--- index.js
+++ index.js
@@ -1,65 +1,110 @@
-let pass1 = document.getElementById('passWord1');
-let pass2 = document.getElementById('passWord2');
-let pass3 = document.getElementById('passWord3');
-let pass4 = document.getElementById('passWord4');
-let tamanho = document.getElementById('tam-pass');
+let pass1 = document.getElementById("passWord1");
+let pass2 = document.getElementById("passWord2");
+let pass3 = document.getElementById("passWord3");
+let pass4 = document.getElementById("passWord4");
+let tamanho = document.getElementById("tam-pass");
 
-let inputCollection = document.getElementsByTagName('input')
-
-
-
-
+let inputCollection = document.getElementsByTagName("input");
 let caracter = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '#', '$', '^', '&', '*', '~'
-]
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "w",
+  "x",
+  "y",
+  "z",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "0",
+  "!",
+  "#",
+  "$",
+  "^",
+  "&",
+  "*",
+  "~",
+];
+
+document.getElementById("btn-generatePass").addEventListener("click", (e) => {
+  e.preventDefault();
+  gerarPass();
+});
 
 function gerarPass() {
-    tam = tamanho.value;
-    console.log(tam)
-    let passW1 = "";
-    let passW2 = "";
-    let passW3 = "";
-    let passW4 = "";
-    for (let i = 0; i < tam; i++) {
-        passW1 += caracter[Math.floor(Math.random() * caracter.length)]
-        passW2 += caracter[Math.floor(Math.random() * caracter.length)]
-        passW3 += caracter[Math.floor(Math.random() * caracter.length)]
-        passW4 += caracter[Math.floor(Math.random() * caracter.length)]
-    }
+  tam = tamanho.value;
+  const rangeError = document.getElementById("rangeError");
+  if (tam < 1 || tam > 12) {
+    rangeError.innerText = "The value must be between 1 and 12";
+    return;
+  }
+  rangeError.innerText = "";
+  let passW1 = "";
+  let passW2 = "";
+  let passW3 = "";
+  let passW4 = "";
+  for (let i = 0; i < tam; i++) {
+    passW1 += caracter[Math.floor(Math.random() * caracter.length)];
+    passW2 += caracter[Math.floor(Math.random() * caracter.length)];
+    passW3 += caracter[Math.floor(Math.random() * caracter.length)];
+    passW4 += caracter[Math.floor(Math.random() * caracter.length)];
+  }
 
-    pass1.value = passW1;
-    pass2.value = passW2;
-    pass3.value = passW3;
-    pass4.value = passW4;
+  pass1.value = passW1;
+  pass2.value = passW2;
+  pass3.value = passW3;
+  pass4.value = passW4;
 }
 
-
 function copyToClipBoard(input) {
-    // Select the text field
-    input.select();
-    input.setSelectionRange(0, 99999); // For mobile devices
+  // Select the text field
+  input.select();
+  input.setSelectionRange(0, 99999); // For mobile devices
 
-    // Copy the text inside the text field
-    navigator.clipboard.writeText(input.value);
+  // Copy the text inside the text field
+  navigator.clipboard.writeText(input.value);
 
-    // Alert the copied text
-    alert("Copied the text: " + input.value);
+  // Alert the copied text
+  outFunc(input.value);
 }
 
 for (let i = 0; i < inputCollection.length; i++) {
-    if (i > 0) {
-
-        inputCollection[i].addEventListener('click', () => {
-            copyToClipBoard(inputCollection[i])
-            console.log(inputCollection[i].value);
-        })
-    }
-
+  if (i > 0) {
+    inputCollection[i].addEventListener("click", () => {
+      copyToClipBoard(inputCollection[i]);
+      console.log(inputCollection[i].value);
+    });
+  }
 }
 
-
-
-function outFunc() {
-    var tooltip = document.getElementById("myTooltip");
-    tooltip.innerHTML = "Copy to clipboard";
+function outFunc(text) {
+  var tooltip = document.getElementById("myTooltip");
+  tooltip.innerHTML = `[${text}] Copied to clipboard`;
+  setTimeout(() => {
+    tooltip.innerHTML = "";
+  }, 2000);
 }
